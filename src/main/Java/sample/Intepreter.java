b595151fb40abf8748d71f5cbd838cbd8c934785
rajeerc <rajeerc@gmail.com>
2013-09-12 13:08:05 +0530

    Update Intepreter.java

--- src/main/Java/sample/Intepreter.java
+++ src/main/Java/sample/Intepreter.java
@@ -1,38 +1,55 @@
-
 package sample;
+
+import org.joda.time.DateTime;
 
 /**
  *
  * @author Rajith
  */
-public class Intepreter {
-    
+public class Interpreter {
 
-    public NICProp createBmiProp (String id) throws InvalidNICPropException{
-        
+    private DateTime birthday;
+    private boolean voter;
+    private String gender;
+    
+    
+    
+    public void interpret (NICProp nicProp){
+        //the interpretting logic is inserted here
+    }
+
+    public NICProp createNICProp(String id) throws InvalidNICPropException {
+
         try {
             return new NICProp(id);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw new InvalidNICPropException();
         }
     }
-    
-    public boolean validate (NICProp nicProp) {
-        
-        if (nicProp.getId().length()!=10){
+
+    public boolean validate(NICProp nicProp) {
+
+        if (nicProp.getId().length() != 10) {
             return false;
         }
-        
-        try{
+
+        try {
             Long.parseLong(nicProp.getId().substring(0, 9));
             return true;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
     
-    
-    
+    public DateTime getBirthday() {
+        return birthday;
+    }
+
+    public boolean isVoter() {
+        return voter;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
